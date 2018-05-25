b21d5436ada23784b46deeadb98868733d61446d
AKASH YADAV <aakash28399@gmail.com>
2018-05-25 23:53:49 +0530

    Create Egg Dropping Problem.java

---
+++ Egg Dropping Problem.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+class EggDrop{
+
+	//https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle/0
+
+	public static Scanner scn = new Scanner(System.in);
+
+	public static void main (String[] args) throws java.lang.Exception{
+
+		EggDrop ED = new EggDrop();
+
+		int eggs = 100;
+		int floors = 2;
+        
+		int ans = ED.dropEggs(eggs, floors);
+
+		System.out.println(ans);
+	}
+	
+	public static int dropEggs(int eggs, int floors){
+	    
+	    if(eggs == 1)
+	        return floors;
+
+	    if(floors == 0)
+	    	return 0;
+	        
+	    int min = Integer.MAX_VALUE;
+	    
+	    //try dropping at every floor
+	    for(int i = 1; i <= floors; i++){
+
+	    	int 
+	        min = Math.min(min, (1 + Math.max(dropEggs(N, K-i), dropEggs(N-1,K-1))));
+	    }
+	
+	    return min;
+	}
+}
